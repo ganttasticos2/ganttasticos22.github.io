bf8d11700b418bcb110834a133aee63f98bcd0c9
AngelMendieta <jesusangelmendieta@gmail.com>
2026-02-11 22:56:24 -0600

    Nueva version 1.12

--- sw.js
+++ sw.js
@@ -15,7 +15,7 @@
  * conoce como número menor y se cambia cuando se realizan
  * modificaciones menores.
  */
-const VERSION = "1.11"
+const VERSION = "1.12"
 
 /**
  * Nombre de la carpeta de caché.
@@ -74,7 +74,7 @@ const ARCHIVOS = [
  "js/lib/resaltaSiEstasEn.js",
  "js/lib/custom/md-app-bar.js",
  "ungap/custom-elements.js",
- "./"
+ "/"
 ]
 
 // Verifica si el código corre dentro de un service worker.
